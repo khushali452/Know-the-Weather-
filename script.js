70d3afb228da9c1acf02dd629b280de6534b743e
Khushali Vasani <62420933+khushali452@users.noreply.github.com>
2021-04-25 13:10:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
 
     var typed = new Typed('.typed', {
-        strings: ['Hello,', 'You can find weather info right here !!'],
+        strings: ['Hello,', 'You can find weather info right here!'],
         smartBackspace: true,
         loop: true,
         typeSpeed: 100,
@@ -27,7 +27,7 @@ $(document).ready(function() {
                         </div>
                 </div>
             </div>
-            </div>);
+            </div>`);
         });
 
     })
